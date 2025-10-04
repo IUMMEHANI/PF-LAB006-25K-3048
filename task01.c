fd47eb325f635f992d2315baf33fc33e3507145f
UMM E HANI <k253048@nu.edu.pk>
2025-10-04 09:13:46 -0700

    task01.c

--- task01.c
+++ task01.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 int main () {
 	int sum = 0;
-	int variable = 0;
+	int value = 0;
 	do {
-		printf("Enter variables for sum and to stop enter 0 and see calculated sum: ");
-		scanf("%d" , &variable);
-		if (variable != 0) {
-			sum = variable + sum;
+		printf("Enter values for sum and to stop enter 0 and see calculated sum: ");
+		scanf("%d" , &value);
+		if (value != 0) {
+			sum = value + sum;
 		}
 		printf("SUM = %d\n" , sum);
-   	} while (variable != 0);
+   	} while (value != 0);
 	printf("FINAL SUM = %d\n" , sum);
 	return 0;
 }
+
